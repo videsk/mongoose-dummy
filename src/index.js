3bf57424dc9fbeee541ad5609b3c0ba3bb7fe541
Matias Lopez Diaz <matiaslopezd@gmail.com>
2021-11-30 09:24:36 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C) CURRENT_YEAR by Videsk - All Rights Reserved
+ * @name LIBRARY_NAME
+ * @author Videsk
+ * @license LICENSE
+ * Written by AUTHOR_LIBRARY
+ *
+ * DESCRIPTION_LIBRARY
+ *
+*/
+
+class MyLibrary {
+
+}
+
+module.exports = MyLibrary;
